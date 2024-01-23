e4d5902288ef4fbdd2f796f6508c07720890e48c
Brandon <brandonbgrayson@gmail.com>
2024-01-23 00:26:23 -0500

    redux store modified to be created during request with makestore function

--- app/components/PokeTable/PokeDex.tsx
+++ app/components/PokeTable/PokeDex.tsx
@@ -15,24 +15,25 @@ interface PokeDexProps {
 
 interface Pokemon extends RawResults {
   id?: number;
+  hasBeenFound?: string;
 }
+
+// pokedex needs to know if a pokemon has been found ~ Update functionality
 
 export default function PokeDex({ data }: PokeDexProps) {
   const [rows, setRows] = useState<Pokemon[]>([]);
 
-  // data currently is an array of objects with a name and url property. types between rows and data currently don't match
-  // I need a row function that will return an array of objects in the format of an ID field and the Name field from the data
-
   useEffect(() => {
-    const indexedRows = data.map((pokemon: Pokemon, index) => {
-      return { ...pokemon, id: index };
+    const pokemon = data.map((poke, index) => {
+      return { ...poke, id: index };
     });
-    setRows(indexedRows);
+    setRows(pokemon);
   }, [data]);
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID" },
     { field: "name", headerName: "Pokemon Name" },
+    { field: "encountered", headerName: "Encountered" },
   ];
 
   return (
