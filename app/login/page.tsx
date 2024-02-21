93327dfb5f812f34941543b83f8db7daf3950dd2
Brandon Grayson <brandonbgrayson@gmail.com>
2024-02-20 19:29:53 -0500

    login page refactored to be a form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,56 @@
 'use client'
-import { Box, Typography, TextField, Button } from "@mui/material"
+import { Box, Typography, TextField, Button, Stack } from "@mui/material"
 import { useState } from "react"
 import { userLogin } from "../actions"
+import { redirect } from "next/navigation"
+
 
 export default function Login() {
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
 
     return (
-        <Box sx={{ height: '100vh', width: '100vw', display: 'flex', justifyContent: "center" }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '200px',}}>
+        <Box sx={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', marginTop: '15vh'}} >
+            <Stack spacing={2}>
+            <Typography
+                fontSize="4em"
+                fontWeight="bold"
+                >
+                Login
+            </Typography>
+            <form method="post" action={() => {
+                 userLogin(username, password) 
+                redirect('/')
+                }}>
+                <Stack spacing={2}>
+                <TextField
+                    id="standard-basic"
+                    label="UserName"
+                    variant="standard"
+                    value={username}
+                    onChange={(event) => setUserName(event.target.value)}
+                    sx={{ marginTop: '30px', width: '20vw' }}
+                />
+                <TextField
+                    id="standard-basic"
+                    label="Password"
+                    variant="standard"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                    sx={{ marginTop: '30px', width: '20vw' }}
+                />
+                <Button
+                    variant="contained"
+                    sx={{ width: '40px', marginTop: '30px' }}
+                    type="submit"
+                >Login
+                </Button>
+                </Stack>
+
+            </form>
+            </Stack>
+
+            {/* <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '200px'}}>
                 <Typography
                 fontSize="4em"
                 fontWeight="bold"
@@ -38,8 +79,7 @@ export default function Login() {
                  onClick={() => userLogin(username, password)}
                  >Login
                  </Button>
-            </Box>
-
+            </Box> */}
         </Box>
     )
 }
