2bdd374d1f3b248de2cf0e3ede3f13206c39e92f
Brandon Grayson <brandonbgrayson@gmail.com>
2024-02-22 22:59:16 -0500

    login textfields input validation

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Box, Typography, TextField, Button, Stack } from "@mui/material"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { userLogin } from "../actions"
 import { redirect } from "next/navigation"
 import { Nav } from "../components/Nav"
@@ -8,6 +8,7 @@ import { Nav } from "../components/Nav"
 export default function Login() {
     const [username, setUserName] = useState("")
     const [password, setPassword] = useState("")
+    const [inputError, setInputError] = useState(false)
 
     return (
         <Box sx={{width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', marginTop: '15vh'}} >
@@ -18,7 +19,10 @@ export default function Login() {
                 >
                 Login
             </Typography>
-            <form method="post" action={() => {
+            <form action={() => {
+                if (username === "" || password === "") {
+                    setInputError(true)
+                }
                  userLogin(username, password) 
                 redirect('/')
                 }}>
@@ -27,6 +31,8 @@ export default function Login() {
                     id="standard-basic"
                     label="UserName"
                     variant="standard"
+                    required
+                    error={inputError}
                     value={username}
                     onChange={(event) => setUserName(event.target.value)}
                     sx={{ marginTop: '30px', width: '20vw' }}
@@ -35,6 +41,9 @@ export default function Login() {
                     id="standard-basic"
                     label="Password"
                     variant="standard"
+                    required
+                    type="password"
+                    error={inputError}
                     value={password}
                     onChange={(event) => setPassword(event.target.value)}
                     sx={{ marginTop: '30px', width: '20vw' }}
@@ -49,37 +58,6 @@ export default function Login() {
 
             </form>
             </Stack>
-
-            {/* <Box sx={{ display: 'flex', flexDirection: 'column', marginTop: '200px'}}>
-                <Typography
-                fontSize="4em"
-                fontWeight="bold"
-                 sx={{ marginRight: "auto", marginLeft: "auto",}}>
-                    Login
-                </Typography>
-                <TextField
-                 id="standard-basic" 
-                 label="UserName" 
-                 variant="standard" 
-                 value={username} 
-                 onChange={(event) => setUserName(event.target.value)} 
-                 sx={{marginTop: '30px', width: '40vw'}}
-                 />
-                <TextField
-                 id="standard-basic" 
-                 label="Password" 
-                 variant="standard" 
-                 value={password} 
-                 onChange={(event) => setPassword(event.target.value)} 
-                 sx={{marginTop: '30px'}}
-                 />
-                 <Button 
-                 variant="contained" 
-                 sx={{width: '40px', marginTop: '30px'}}
-                 onClick={() => userLogin(username, password)}
-                 >Login
-                 </Button>
-            </Box> */}
         </Box>
     )
 }
