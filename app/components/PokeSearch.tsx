561ff0d724203546b818d3fd83f482c3ff06d815
Brandon Grayson <brandonbgrayson@gmail.com>
2024-02-28 19:51:58 -0500

    pokemon data added to card component

--- app/components/PokeSearch.tsx
+++ app/components/PokeSearch.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, Stack, TextField, Card, CardActions, CardContent, CardMedia, } from "@mui/material"
+import { Button, Stack, TextField, Card, CardActions, CardContent, CardMedia, Typography, } from "@mui/material"
 import { useState } from "react"
 import { searchPokemon } from "../actions"
 import { PokemonResult } from "../schemas/pokemon"
@@ -12,16 +12,27 @@ export default function PokeSearch() {
         image: '',
         type: ''
     })
-
+    console.log('pokemon', pokemon)
     return (
         <>
-            <Card sx={{ maxWidth: 345 }}>
+            {
+                pokemon?.name ? 
+                <Card sx={{ maxWidth: 345, marginTop: '20px' }}>
                 <CardMedia
                     sx={{ height: 140 }}
-                    image={pokemon?.image}
-                    title="green iguana"
+                    image={pokemon.image}
+                    title="Pokemon"
                 />
+                <CardContent>
+                    <Typography>Name: {pokemon.name}</Typography>
+                    <Typography>Type: {pokemon.type}</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button variant="contained">Add To Pokedex</Button>
+                </CardActions>
             </Card>
+            : null
+            }
             <Stack spacing={2} sx={{ marginTop: '20px', width: '400px' }}>
                 <TextField
                     value={search}
