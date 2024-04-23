25bd5c9c3d2245042091c741d9300ec49c4516df
Brandon Grayson <brandonbgrayson@gmail.com>
2024-04-23 19:27:31 -0400

    Added datagrid to Pokedex component with starter columns and rows

--- app/components/PokeTable/PokeDex.tsx
+++ app/components/PokeTable/PokeDex.tsx
@@ -4,14 +4,15 @@ import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import { Box, Typography } from "@mui/material";
 import PokeSearch from "../PokeSearch";
 import { Pokemon } from "@/app/schemas/pokemon";
+import { useEffect } from "react";
 
 // pokedex needs to know if a pokemon has been found ~ Update functionality
 
 interface PokedexProps {
-  pokemon: Pokemon
+  pokemon: Pokemon[]
 }
 
-export default function PokeDex({pokemon}: PokedexProps) {
+export default function PokeDex({ pokemon }: PokedexProps) {
 
   const columns: GridColDef[] = [
     { field: "id", headerName: "ID" },
@@ -23,12 +24,21 @@ export default function PokeDex({pokemon}: PokedexProps) {
     { field: "image", headerName: "Image" },
   ];
 
+  const rows: GridRowsProp<Pokemon> = pokemon
+
+  useEffect(() => {
+
+  }, [pokemon])
+
+  console.log('rows', rows)
+
   return (
     <>
       <Typography>PokeDex</Typography>
+
+      <Box sx={{ width: 600, height: 600 }}>
+        <DataGrid rows={rows} columns={columns} />
+      </Box>
     </>
-    // <Box sx={{ width: 600, height: 600 }}>
-    //   {/* <DataGrid rows={rows} columns={columns} /> */}
-    // </Box>
   );
 }
